ba57ebcf7d66430cb9fc1f79141959205514a192
pepper24Bot <jen@robolabs.biz>
2024-02-07 19:37:35 +0800

    fix root wallet component responsive layout

--- src/app/components/Sections/RootWallet/RootWallet.tsx
+++ src/app/components/Sections/RootWallet/RootWallet.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Container, Period, SubTitle, Title } from "./StyledRootWallet";
+import {
+  Container,
+  LeftPanel,
+  RightPanel,
+  SubTitle,
+  Title,
+} from "./StyledRootWallet";
 import {
   FlexCenter,
   ImageContainer,
@@ -11,33 +17,31 @@ import Image from "next/image";
 export const RootWallet: React.FC = () => {
   return (
     <PageSection>
-      <FlexCenter>
-        <Container>
-          <div className="slide-left-delay-1 reveal">
-            <Period>.</Period>
-            <Title>root</Title>
-          </div>
-          <div className="slide-right-delay-2 reveal">
-            <ImageContainer>
-              <Image
-                src="/icons/wallet.svg"
-                alt="Your-RNS"
-                width={115}
-                height={100}
-                style={{
-                  marginBottom: "24px",
-                  width: "15vw",
-                  maxWidth: "115px",
-                }}
-              />
-            </ImageContainer>
-            <SubTitle>
-              Your .root extension is used to easily identify your FuturePass
-              wallet address and act as your identity across the open metaverse
-            </SubTitle>
-          </div>
-        </Container>
-      </FlexCenter>
+      <Container>
+        <LeftPanel className="slide-left-delay-1 reveal">
+          <Title>.root</Title>
+        </LeftPanel>
+        <RightPanel className="slide-right-delay-2 reveal">
+          <ImageContainer>
+            <Image
+              src="/icons/wallet.svg"
+              alt="Your-RNS"
+              width={115}
+              height={100}
+              style={{
+                marginBottom: "24px",
+                width: "15vw",
+                maxWidth: "115px",
+              }}
+            />
+          </ImageContainer>
+          <SubTitle>
+            Your .root extension is used to easily identify your FuturePass
+            wallet address and act as your identity across the open metaverse
+          </SubTitle>
+        </RightPanel>
+      </Container>
+      <FlexCenter></FlexCenter>
     </PageSection>
   );
 };
--- src/app/components/Sections/RootWallet/StyledRootWallet.tsx
+++ src/app/components/Sections/RootWallet/StyledRootWallet.tsx
@@ -1,52 +1,50 @@
 import styled from "styled-components";
-import { BaseFont, FlexCenter, PageSubTitle } from "../../Global/StyledGlobal";
+import {
+  BaseFont,
+  Flex,
+  FlexCenter,
+  PageSubTitle,
+} from "../../Global/StyledGlobal";
 
 export const Container = styled(FlexCenter)`
   align-items: end;
-  width: 75vw;
+  width: 100%;
   max-width: 500px;
-  padding: 48px 24px;
-  margin: 16px 0 144px 0;
+  margin: 0 0 128px 0;
+
+  @media only screen and (max-width: 600px) {
+    padding: 0 16px 48px 16px;
+    max-width: 350px;
+  }
 `;
+
+export const LeftPanel = styled(Flex)``;
+
+export const RightPanel = styled.div``;
 
 export const Title = styled(BaseFont)`
   font-size: calc(16px + 5vmax);
   text-orientation: upright;
   writing-mode: vertical-rl;
-  line-height: 75px;
-  letter-spacing: -30px;
+  line-height: normal;
+  letter-spacing: -40px;
   margin-bottom: 8px;
-  padding-right: 16px;
 
   @media only screen and (max-width: 500px) {
-    margin-bottom: 16px;
+    letter-spacing: -20px;
   }
 
-  @media only screen and (min-width: 600px) and (max-width: 1000px) {
-    font-size: 75px;
-    padding-right: 24px;
+  @media only screen and (min-width: 800px) and (max-width: 1000px) {
+    letter-spacing: -25px;
   }
 
-  @media only screen and (min-width: 1000px) {
+  @media only screen and (min-width: 1000px) and (max-width: 1200px) {
+    letter-spacing: -30px;
+    font-size: 100px;
+  }
+
+  @media only screen and (min-width: 1200px) {
     font-size: 125px;
-    padding-right: 48px;
-  }
-`;
-
-export const Period = styled(BaseFont)`
-  font-size: calc(16px + 5vmax);
-  text-align: center;
-  line-height: 50px;
-  padding-right: 16px;
-
-  @media only screen and (min-width: 500px) and (max-width: 1000px) {
-    font-size: 75px;
-    padding-right: 24px;
-  }
-
-  @media only screen and (min-width: 1000px) {
-    font-size: 125px;
-    padding-right: 48px;
   }
 `;
 
